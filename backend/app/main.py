b4a795c8504b4357b3a0cf8be849fba249e90cda
Neelraj D Neharkar <neharkarneelraj@gmail.com>
2026-07-18 21:52:45 +0530

    Day-3: Added routes and Pydantic model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.schemas.student import Student
 
 app = FastAPI(
     title="SkillSync AI API",
@@ -10,4 +11,36 @@ app = FastAPI(
 def home():
     return {
         "message": "Welcome to SkillSync AI 🚀"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "Running Successfully"
+    }
+
+@app.get("/about")
+def about():
+    return {
+        "project": "SkillSync AI",
+        "version": "1.0.0"
+    }
+
+@app.get("/student/{student_id}")
+def student(student_id: int):
+    return {
+        "student_id": student_id
+    }
+
+@app.get("/search")
+def search(name: str):
+    return {
+        "Searching": name
+    }
+
+@app.post("/student")
+def create_student(student: Student):
+    return {
+        "message":"Student Created Successfully",
+        "data":student
     }

37f8d5df96d0eed5a1f0e3624897ab6c864f12a7
Neelraj D Neharkar <neharkarneelraj@gmail.com>
2026-07-24 11:37:29 +0530

    CRUD operations done

--- backend/app/routers/student.py
+++ backend/app/routers/student.py
@@ -25,7 +25,66 @@ def create_student(student: Student):
 
 @router.get("/students")
 def get_students():
-    return{
-        "total_students":len(students),
-        "students":students
+
+    students = list(students_collection.find())
+
+    for student in students:
+        student["_id"] = str(student["_id"])
+
+    return {
+        "total_students": len(students),
+        "students": students
+    }
+
+@router.get("/student/name/{name}")
+def get_student_by_name(name: str):
+
+    student = students_collection.find_one({"name": name})
+
+    if student is None:
+        return {
+            "message": "Student Not Found"
+        }
+
+    student["_id"] = str(student["_id"])
+
+    return student
+
+@router.put("/student/{name}")
+def update_student(name: str, age: int, Course:str):
+    result = students_collection.update_one(
+        {"name": name},
+        {
+            "$set": {
+                "age": age,
+                "Course":Course
+            }
+        }
+    )
+
+    if result.modified_count == 0:
+        return {
+            "message": "Student Not Found"
+        }
+    return {
+        "message": "Student Updated Successfully"
+    }
+
+@router.delete("/student/{name}")
+def delete_student(name: str):
+
+    result = students_collection.delete_one(
+        {
+            "name": name
+        }
+    )
+
+    if result.deleted_count == 0:
+
+        return {
+            "message": "Student Not Found"
+        }
+
+    return {
+        "message": "Student Deleted Successfully"
     }
